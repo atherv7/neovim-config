c0744030e7a56c3a737a37dd3b218e48a5648f86
atharv <atharvghone@gmail.com>
2026-03-21 21:04:42 -0400

    theme changes

--- lua/plugins/all-themes.lua
+++ lua/plugins/all-themes.lua
@@ -7,8 +7,15 @@ return {
       require("oasis").setup()
       vim.cmd.colorscheme("oasis-abyss")
 
-      vim.api.nvim_set_hl(0, "NeoTreeNormal", { bg = "NONE" })
-      vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { bg = "NONE" })
+      vim.api.nvim_set_hl(0, "NeoTreeNormal", { link = "Normal" })
+      vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { link = "NormalNC" })
+
+      vim.api.nvim_set_hl(0, "NeoTreeFileName", { link = "Normal" })
+      vim.api.nvim_set_hl(0, "NeoTreeDirectoryName", { link = "Directory" })
+      vim.api.nvim_set_hl(0, "NeoTreeRootName", { link = "Title" })
+      vim.api.nvim_set_hl(0, "NeoTreeGitAdded", { link = "DiffAdd" })
+      vim.api.nvim_set_hl(0, "NeoTreeGitModified", { link = "DiffChange" })
+      vim.api.nvim_set_hl(0, "NeoTreeGitDeleted", { link = "DiffDelete" })
     end
   }
 }
